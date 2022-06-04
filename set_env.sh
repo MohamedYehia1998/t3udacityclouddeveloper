2ae3f18e52af39ebad422f02d5b4817e88400deb
ge37zat <mohamedyehia1998@gmail.com>
2022-06-04 08:05:08 +0200

    Microservices built

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=mohamedgh1998
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=mypgdb1998.czky3jsl4ase.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::arn:aws:s3:::t3mohamedgh1998
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
